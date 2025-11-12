6b71a0f003dece3e38a97188c0d0a973a70de49c
JUAN CARLOS ALVAREZ A <jeici0507@gmail.com>
2025-11-11 23:03:16 -0500

    Add files via upload

---
+++ Bucle “for” – Promedio de notas.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de notas: ");
+        int n = int.Parse(Console.ReadLine());
+        double suma = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"Ingrese la nota {i}: ");
+            suma += double.Parse(Console.ReadLine());
+        }
+
+        double promedio = suma / n;
+        Console.WriteLine($"Promedio final: {promedio}");
+
+
+
+
+
+    }
+}
