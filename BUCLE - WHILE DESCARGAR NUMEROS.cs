e9e79185051fd4bd2894e7199e41c849cf3afbc6
JUAN CARLOS ALVAREZ A <jeici0507@gmail.com>
2025-11-11 23:07:42 -0500

    Add files via upload

---
+++ BUCLE - WHILE DESCARGAR NUMEROS.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+        while (i <= 20)
+        {
+            if (i % 2 == 0)
+                Console.Write(i + " ");
+            i++;
+        }
+    }
+}
