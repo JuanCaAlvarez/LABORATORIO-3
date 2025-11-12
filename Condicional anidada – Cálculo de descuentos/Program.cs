56a2de371225f0cee82f357f352fe4a2fc820a15
JUAN CARLOS ALVAREZ AYALA <jeici0507@gmail.com>
2025-11-11 22:38:41 -0500

    Agregar archivos de proyecto.

---
+++ Condicional anidada – Cálculo de descuentos/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el monto de la compra: ");
+        double monto = double.Parse(Console.ReadLine());
+        double descuento;
+
+        if (monto > 500)
+        {
+            descuento = 0.20;
+        }
+        else if (monto > 200)
+        {
+            descuento = 0.10;
+        }
+        else
+        {
+            descuento = 0.05;
+        }
+
+        double total = monto - (monto * descuento);
+        Console.WriteLine($"Descuento aplicado: {descuento * 100}%");
+        Console.WriteLine($"Total a pagar: {total}");
+    }
+}
